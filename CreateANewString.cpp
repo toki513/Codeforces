69f2f019a092b2898d9652b4d8fe2891d6f747a2
Toki Tahmid Khan <122171015+toki513@users.noreply.github.com>
2024-10-02 23:40:37 +0600

    Add files via upload

---
+++ CreateANewString.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string s,t;
+	cin>>s>>t;
+
+	int S=s.length();
+	int T=t.length();
+
+	cout<<S<<" "<<T<<endl;
+	cout<<s<<" "<<t<<endl;
+}
